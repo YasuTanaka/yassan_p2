496e74deaabf48b8b296c7bb464deccda2a00373
Yasuto Tanaka <yasuto.tanaka@game8.jp>
2018-07-03 18:08:36 +0900

    reration_category

--- app/models/archive.rb
+++ app/models/archive.rb
@@ -1,2 +1,3 @@
 class Archive < ApplicationRecord
+   belongs_to :genre, class_name: "Category", foreign_key: "category_id"
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,3 @@
 class Category < ApplicationRecord
+  has_many :archives, dependent: :destroy
 end
